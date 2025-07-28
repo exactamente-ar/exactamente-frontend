1ec6be1af0b9f9730ef079c3db74ae825cba0b89
Juan Peñalba <juantandil123@gmail.com>
2025-07-28 20:56:48 -0300

    feat: google search console configurado

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,18 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: 'https://exactamente.com.ar',
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
