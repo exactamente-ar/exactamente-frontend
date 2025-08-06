107298247e77b9defccc9829922ee39fcaf4b2be
Juan Peñalba <juantandil123@gmail.com>
2025-08-06 17:30:41 -0300

    recursos ordenados por año

--- frontend/src/hooks/useResources.ts
+++ frontend/src/hooks/useResources.ts
@@ -20,12 +20,17 @@ export const useResources = (id: string, type: StringResource) => {
 
       try {
         const { data, error } = await getByIdResources(id, type);
+        const sortedResources = [...data].sort((a, b) => {
+          const yearA = parseInt(a.title.split(' ')[1], 10);
+          const yearB = parseInt(b.title.split(' ')[1], 10);
+          return yearB - yearA; // mayor a menor
+        });
 
         if (error) {
           setError(error);
           setData(null);
         } else {
-          setData(data);
+          setData(sortedResources);
         }
       } catch (err) {
         setError(error);
