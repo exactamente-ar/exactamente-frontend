346c7503b4b363a8dc34b6785daa32d23dbe9df2
Juan Peñalba <89142353+JuanPE44@users.noreply.github.com>
2026-01-20 15:21:00 -0300

    fix: api

--- src/features/resource/services/resource.ts
+++ src/features/resource/services/resource.ts
@@ -1,7 +1,7 @@
 import type { ResourceFetch, StringResource } from '@/features/resource/types/resource';
 import { fetchSheet } from '@/features/resource/utils/fetchSheet';
 
-const RESOURCE_URL = 'https://api.sheetbest.com/sheets/13b4f0d7-e711-4897-afc0-e8bb6fff8759/tabs';
+const RESOURCE_URL = 'https://api.sheetbest.com/sheets/186b9672-f516-4fcc-9ae6-acff56b8c61e/tabs';
 
 export async function getAllResources(type: StringResource) {
   const url = `${RESOURCE_URL}/${type}`;
