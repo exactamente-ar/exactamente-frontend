68cd21c4acfd890727be1847f817ed36f92dd2a5
Juan Peñalba <89142353+JuanPE44@users.noreply.github.com>
2026-05-22 11:29:22 -0300

    Update RESOURCE_URL to new API endpoint

--- src/features/resource/services/resource.ts
+++ src/features/resource/services/resource.ts
@@ -1,7 +1,7 @@
 import type { ResourceFetch, StringResource } from '@/features/resource/types/resource';
 import { fetchSheet } from '@/features/resource/utils/fetchSheet';
 
-const RESOURCE_URL = 'https://api.sheetbest.com/sheets/7b543277-62ad-400f-864c-7434c627bfaa/tabs';
+const RESOURCE_URL = 'https://api.sheetbest.com/sheets/73ddc710-7545-4e81-90f4-b89a43bca7fc/tabs';
 
 export async function getAllResources(type: StringResource) {
   const url = `${RESOURCE_URL}/${type}`;
